e43898589d6fec07d1a28817fe8baec83da8c953
anu yadav <DEVARA.ANU@gmail.com>
2025-10-13 15:22:47 +0000

    Try fixing this error: `Runtime Error: TypeError: (0 , {imported module

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -1,11 +1,6 @@
 
-import {
-  getAuth as getAdminAuth,
-  initializeApp as initializeAdminApp,
-  getApps as getAdminApps,
-  getApp as getAdminApp,
-  ServiceAccount,
-} from "firebase-admin/auth";
+import { initializeApp as initializeAdminApp, getApps as getAdminApps, getApp as getAdminApp, type ServiceAccount } from 'firebase-admin/app';
+import { getAuth as getAdminAuth } from "firebase-admin/auth";
 import { getFirestore as getAdminFirestore } from "firebase-admin/firestore";
 import { getStorage as getAdminStorage } from "firebase-admin/storage";
 import "server-only";
@@ -50,5 +45,6 @@ const adminDb = getAdminFirestore(adminApp);
 const adminStorage = getAdminStorage(adminApp);
 
 export { adminApp, adminAuth, adminDb, adminStorage };
+
 
 
