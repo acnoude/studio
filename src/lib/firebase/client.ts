f3fbe73466fb10d1602525fb9e49ed43e699b3f9
anu yadav <DEVARA.ANU@gmail.com>
2025-10-16 20:25:05 +0000

    failed to build: (error ID: 5f368dcf): {"reason":"Failed Framework Build

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,10 +1,11 @@
+
 "use client";
 
-import { initializeApp, getApps, getApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-import { getFunctions } from "firebase/functions";
+import { initializeApp, getApps, getApp, FirebaseApp } from "firebase/app";
+import { getAuth, Auth } from "firebase/auth";
+import { getFirestore, Firestore } from "firebase/firestore";
+import { getStorage, FirebaseStorage } from "firebase/storage";
+import { getFunctions, Functions } from "firebase/functions";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -16,10 +17,20 @@ const firebaseConfig = {
 };
 
 // Client-side Firebase
-const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app, "hhsilentbid");
-const storage = getStorage(app);
-const functions = getFunctions(app);
+let app: FirebaseApp;
+let auth: Auth;
+let db: Firestore;
+let storage: FirebaseStorage;
+let functions: Functions;
 
+// Prevent Firebase initialization during server-side rendering
+if (typeof window !== "undefined") {
+  app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+  auth = getAuth(app);
+  db = getFirestore(app, "hhsilentbid");
+  storage = getStorage(app);
+  functions = getFunctions(app);
+}
+
+// @ts-ignore
 export { app, auth, db, storage, functions };
